c7caa1cb49529c0ca5d0e4e4404e69760362d43b
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-01-20 05:52:41 +0700

    Create Categori Components

---
+++ components/organisms/OverviewContent/Categori.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { ReactNode } from 'react';
+
+interface CategoriProps {
+  children: ReactNode;
+  value : number;
+  icon: 'icon-dekstop' | 'icon-mobile';
+}
+export default function Categori(props: CategoriProps) {
+  const {
+    children, value, icon,
+  } = props;
+
+  const formmater = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', maximumSignificantDigits: 3 });
+  const newValue = formmater.format(value);
+  return (
+    <div className="col-lg-4 ps-15 pe-15 pb-lg-0 pb-4">
+      <div className="categories-card">
+        <div className="d-flex align-items-center mb-24">
+          <Image src={`/icon/${icon}.svg`} width={60} height={60} alt="icon category" />
+          <p className="color-palette-1 mb-0 ms-12">
+            {children}
+          </p>
+        </div>
+        <div>
+          <p className="text-sm color-palette-2 mb-1">Total Spent</p>
+          <p className="text-2xl color-palette-1 fw-medium m-0">
+            {newValue}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
