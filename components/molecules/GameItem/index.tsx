86cccb19b9c14184a9724bfd8e3de25a6a29ccc0
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-01-20 05:49:55 +0700

    Update Game Item

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -1,29 +1,41 @@
-export default function GameItem() {
+import Image from 'next/image';
+import Link from 'next/link';
+
+export interface GameItemProps {
+  title: string;
+  category: string;
+  thumbnail: '/img/Thumbnail-1.png' | '/img/Thumbnail-2.png' | '/img/Thumbnail-3.png' | '/img/Thumbnail-4.png' | '/img/Thumbnail-5.png';
+}
+export default function GameItem(props: GameItemProps) {
+  const { title, category, thumbnail } = props;
   return (
     <div className="featured-game-card position-relative">
-      <a href="/detail">
-        <div className="blur-sharp">
-          <img
-            src="/img/Thumbnail-1.png"
-            width="205"
-            height="270"
-            alt=""
-          />
-        </div>
-        <div className="cover position-absolute bottom-0 m-32">
-          <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
-            <div className="game-icon mx-auto">
-              <img src="/icon/console.svg" width="54" height="36" alt="console" />
-            </div>
-            <div>
-              <p className="fw-semibold text-white text-xl m-0">
-                Super Mechs
-              </p>
-              <p className="fw-light text-white m-0">Mobile</p>
+      <Link href="/detail">
+        <a>
+          <div className="blur-sharp">
+            <Image
+              className="thumbnail"
+              src={thumbnail}
+              width={205}
+              height={270}
+              alt="thumbnail"
+            />
+          </div>
+          <div className="cover position-absolute bottom-0 m-32">
+            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+              <div className="game-icon mx-auto">
+                <Image src="/icon/console.svg" width={54} height={36} alt="console" />
+              </div>
+              <div>
+                <p className="fw-semibold text-white text-xl m-0">
+                  {title}
+                </p>
+                <p className="fw-light text-white m-0">{category}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </a>
+        </a>
+      </Link>
     </div>
   );
 }
